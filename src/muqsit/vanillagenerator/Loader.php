e69331490f600251930cab34618b9decec242af4
xwertxy <xwertxy@hotmail.com>
2025-07-19 11:45:35 +0800

    changes

--- src/muqsit/vanillagenerator/Loader.php
+++ src/muqsit/vanillagenerator/Loader.php
@@ -11,7 +11,7 @@ use pocketmine\world\generator\GeneratorManager;
 
 final class Loader extends PluginBase{
 
-	public function onLoad() : void{
+	public function onLoad() : void {
 		$generator_manager = GeneratorManager::getInstance();
 		$generator_manager->addGenerator(NetherGenerator::class, "vanilla_nether", fn() => null);
 		$generator_manager->addGenerator(OverworldGenerator::class, "vanilla_overworld", fn() => null);
